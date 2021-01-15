45fbf163bb5f0a6a38a7b50995366fc723ed3af6
Philip Milliorn <scottmilliorn@outlook.com>
2021-01-15 15:16:11 -0800

    OnAcquireItem

--- src/Services/ModuleItemAcquire.cs
+++ src/Services/ModuleItemAcquire.cs
@@ -6,6 +6,7 @@ using NWN.API.Events;
 using NWN.Services;
 
 using NWNX.API;
+using static NWN.API.Events.ModuleEvents;
 using Effect = NWN.API.Effect;
 
 namespace Module
@@ -14,5 +15,14 @@ namespace Module
 
     public class ModuleItemAcquire
     {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        public ModuleItemAcquire(NativeEventService native) =>
+            native.Subscribe<NwModule, ModuleEvents.OnAcquireItem>(NwModule.Instance, OnAcquireItem);
+
+        private static void OnAcquireItem(ModuleEvents.OnAcquireItem acquireItem)
+        {
+            
+        }
     }
-} 
+}
