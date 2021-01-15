1d56b0ef5b2a033232bba508b008f0731a9c1377
Philip Milliorn <scottmilliorn@outlook.com>
2021-01-14 17:32:03 -0800

    break if dm

--- src/Services/ModuleClientLeave.cs
+++ src/Services/ModuleClientLeave.cs
@@ -1,8 +1,11 @@
+using System.Threading.Tasks;
 using NLog;
 
 using NWN.API;
+using NWN.API.Constants;
 using NWN.API.Events;
 using NWN.Services;
+using NWNX.API;
 
 namespace Module
 {
@@ -12,17 +15,36 @@ namespace Module
         private static readonly Logger Log = LogManager.GetCurrentClassLogger();
 
         public ModuleClientLeave(NativeEventService nativeEventService) =>
-            nativeEventService.Subscribe<NwModule, ModuleEvents.OnClientLeave>(NwModule.Instance, OnClientLeave);
+            nativeEventService.Subscribe<NwModule, ModuleEvents.OnClientLeave>(NwModule.Instance, OnClientLeaveAsync);
 
-        private static void OnClientLeave(ModuleEvents.OnClientLeave leave)
+        private async void OnClientLeaveAsync(ModuleEvents.OnClientLeave leave)
         {
+            await ClientPrintLogout(leave);
+
             /* This is to short circuit the rest of this code if we are DM */
             if (leave.Player.IsDM)
             {
                 return;
             }
+        }
 
-            Log.Info(leave.Player.Name);
+        private static async Task ClientPrintLogout(ModuleEvents.OnClientLeave leave)
+        {
+            string colorString = $"\n{"NAME".ColorString(Color.GREEN)}:{leave.Player.Name.ColorString(Color.WHITE)}\n{"ID".ColorString(Color.GREEN)}:{leave.Player.CDKey.ColorString(Color.WHITE)}\n{"BIC".ColorString(Color.GREEN)}:{Player.GetBicFileName(leave.Player).ColorString(Color.WHITE)}";
+            string client = $"NAME:{leave.Player.Name} ID:{leave.Player.CDKey} BIC:{Player.GetBicFileName(leave.Player)}";
+            string clientDM = $"NAME:{leave.Player.Name} ID:{leave.Player.CDKey}";
+
+            if (leave.Player.IsDM)
+            {
+                NwModule.Instance.SendMessageToAllDMs($"\n{"Exiting DM".ColorString(Color.GREEN)}:{colorString}");
+                Log.Info($"DM Exiting:{clientDM}.");
+
+            }
+            else
+            {
+                await NwModule.Instance.SpeakString($"\n{"LOGOUT".ColorString(Color.GREEN)}:{colorString}", TalkVolume.Shout);
+                Log.Info($"LOGOUT:{client}.");
+            }
         }
     }
 }
