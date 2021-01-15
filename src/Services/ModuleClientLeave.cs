57012afa40b44587eb63017b46b1534055d0a7f4
Philip Milliorn <scottmilliorn@outlook.com>
2021-01-14 17:19:31 -0800

    dm check

--- src/Services/ModuleClientLeave.cs
+++ src/Services/ModuleClientLeave.cs
@@ -1,7 +1,28 @@
-namespace Services
+using NLog;
+
+using NWN.API;
+using NWN.API.Events;
+using NWN.Services;
+
+namespace Module
 {
+    [ServiceBinding(typeof(ModuleClientLeave))]
     public class ModuleClientLeave
     {
-        
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        public ModuleClientLeave(NativeEventService nativeEventService) =>
+            nativeEventService.Subscribe<NwModule, ModuleEvents.OnClientLeave>(NwModule.Instance, OnClientLeave);
+
+        private static void OnClientLeave(ModuleEvents.OnClientLeave leave)
+        {
+            /* This is to short circuit the rest of this code if we are DM */
+            if (leave.Player.IsDM)
+            {
+                return;
+            }
+
+            Log.Info(leave.Player.Name);
+        }
     }
 }
