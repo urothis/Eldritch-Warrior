502e6aa3253ffab629e183e5ce38385228e9aea3
Philip Milliorn <scottmilliorn@outlook.com>
2021-01-15 15:07:48 -0800

    ModuleItemAcquire

---
+++ src/Services/ModuleItemAcquire.cs
@@ -0,0 +1,18 @@
+using NLog;
+
+using NWN.API;
+using NWN.API.Constants;
+using NWN.API.Events;
+using NWN.Services;
+
+using NWNX.API;
+using Effect = NWN.API.Effect;
+
+namespace Module
+{
+    [ServiceBinding(typeof(ModuleItemAcquire))]
+
+    public class ModuleItemAcquire
+    {
+    }
+}
