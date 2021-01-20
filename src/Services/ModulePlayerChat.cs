d927094295dd5f79226b476d8e367475166a5547
Philip Milliorn <scottmilliorn@outlook.com>
2021-01-19 16:50:43 -0800

    SetVoice

--- src/Services/ModulePlayerChat.cs
+++ src/Services/ModulePlayerChat.cs
@@ -5,12 +5,16 @@ using NWN.API;
 using NWN.API.Events;
 using NWN.Services;
 
+using NWNX.API;
+
 namespace Module
 {
     [ServiceBinding(typeof(ModulePlayerChat))]
     public class ModulePlayerChat
     {
         private static readonly char wildcard = '!';
+        private static readonly string notReady = "Feature not implemented.";
+
         //private static readonly Logger Log = LogManager.GetCurrentClassLogger();
 
         public ModulePlayerChat(NativeEventService native) =>
@@ -22,20 +26,20 @@ namespace Module
         private static void ChatTools(ModuleEvents.OnPlayerChat chat)
         {
             chat.Message = chat.Message.ToLower();
-            
+
             if (chat.Message.StartsWith(wildcard))
             {
                 chat.Message = chat.Message[1..];
                 string[] chatArray = chat.Message.Split(' ');
 
-                if (chatArray[0].Equals("portrait"))
+                if (chatArray[0].Equals("portrait", System.StringComparison.InvariantCultureIgnoreCase))
                 {
                     SetPortrait(chat, chatArray);
                     return;
                 }
-                if (chatArray[0].Equals("voice"))
+                if (chatArray[0].Equals("voice", System.StringComparison.InvariantCultureIgnoreCase))
                 {
-                    SetPortrait(chat, chatArray);
+                    SetVoice(chat, chatArray);
                     return;
                 }
             }
@@ -46,6 +50,14 @@ namespace Module
             if (int.TryParse(chatArray[1], out int n))
             {
                 chat.Sender.PortraitId = n;
+            }
+        }
+
+        private static void SetVoice(ModuleEvents.OnPlayerChat chat, string[] chatArray)
+        {
+            if (int.TryParse(chatArray[1], out _))
+            {
+                chat.Message = notReady;
             }
         }
     }
