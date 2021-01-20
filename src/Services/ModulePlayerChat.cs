b6f09c1ecf94a5d9793224006b03a90309792065
Philip Milliorn <scottmilliorn@outlook.com>
2021-01-19 16:16:21 -0800

    SetPortrait

--- src/Services/ModulePlayerChat.cs
+++ src/Services/ModulePlayerChat.cs
@@ -1,4 +1,5 @@
-using NLog;
+using System.Text;
+//using NLog;
 
 using NWN.API;
 using NWN.API.Events;
@@ -10,22 +11,34 @@ namespace Module
     public class ModulePlayerChat
     {
         private static readonly char wildcard = '!';
-        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+        //private static readonly Logger Log = LogManager.GetCurrentClassLogger();
 
         public ModulePlayerChat(NativeEventService native) =>
             native.Subscribe<NwModule, ModuleEvents.OnPlayerChat>(NwModule.Instance, PlayerChat);
 
         // handle chat commands
-        private void PlayerChat(ModuleEvents.OnPlayerChat playerChat)
-        {
-            ChatTools(playerChat);
-        }
+        private void PlayerChat(ModuleEvents.OnPlayerChat playerChat) => ChatTools(playerChat);
 
         private static void ChatTools(ModuleEvents.OnPlayerChat chat)
         {
             if (chat.Message.StartsWith(wildcard))
             {
+                chat.Message = chat.Message[1..];
+                string[] chatArray = chat.Message.Split(' ');
 
+                if (chatArray[0].Equals("portrait"))
+                {
+                    SetPortrait(chat, chatArray);
+                    return;
+                }
+            }
+        }
+
+        private static void SetPortrait(ModuleEvents.OnPlayerChat chat, string[] chatArray)
+        {
+            if (int.TryParse(chatArray[1], out int n))
+            {
+                chat.Sender.PortraitId = n;
             }
         }
     }
