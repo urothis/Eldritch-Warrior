e5923fea757d0e845ef5e01c1554187d74cc5007
Philip Milliorn <scottmilliorn@outlook.com>
2021-01-24 13:03:56 -0800

    PlayerHasDied

--- src/Services/ModuleDying.cs
+++ src/Services/ModuleDying.cs
@@ -1,4 +1,4 @@
-using NLog;
+//using NLog;
 
 using NWN.API;
 using NWN.API.Constants;
@@ -10,29 +10,31 @@ namespace Module
     [ServiceBinding(typeof(ModulePlayerDying))]
     public class ModulePlayerDying
     {
-        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+        //private static readonly Logger Log = LogManager.GetCurrentClassLogger();
 
         public ModulePlayerDying(NativeEventService native) =>
             native.Subscribe<NwModule, ModuleEvents.OnPlayerDying>(NwModule.Instance, OnPlayerDying);
 
-        private static void OnPlayerDying(ModuleEvents.OnPlayerDying dying)
-        {
-            BleedOut(dying, 1);
-        }
+        private static void OnPlayerDying(ModuleEvents.OnPlayerDying dying) => BleedOut(dying, 1);
+
+        private static bool PlayerIsDead(ModuleEvents.OnPlayerDying dying) => dying.Player.HP <= 10;
 
         private static void BleedOut(ModuleEvents.OnPlayerDying dying, int bleedAmount)
         {
             dying.Player.HP = bleedAmount > 0 ? dying.Player.HP-- : dying.Player.HP++;
 
-            ScreamOnDeath(dying);
+            if (PlayerIsDead(dying))
+            {
+                PlayerHasDied(dying);
+                return;
+            }
         }
 
-        private static void ScreamOnDeath(ModuleEvents.OnPlayerDying dying)
+        private static void PlayerHasDied(ModuleEvents.OnPlayerDying dying)
         {
-            if (dying.Player.HP <= 10)
-            {
-                dying.Player.PlayVoiceChat(VoiceChatType.Death);
-            }
+            dying.Player.PlayVoiceChat(VoiceChatType.Death);
+            dying.Player.ApplyEffect(EffectDuration.Instant, Effect.VisualEffect(VfxType.ImpDeath));
+            dying.Player.ApplyEffect(EffectDuration.Instant, Effect.Death());
         }
     }
 }
