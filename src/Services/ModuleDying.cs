237334253864d7ebf90de8f6ea1ed91a72a98476
Philip Milliorn <scottmilliorn@outlook.com>
2021-01-24 19:11:21 -0800

    Bleed fix later

--- src/Services/ModuleDying.cs
+++ src/Services/ModuleDying.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Threading.Tasks;
 using NLog;
-
 using NWN.API;
 using NWN.API.Constants;
 using NWN.API.Events;
@@ -9,66 +8,49 @@ using NWN.Services;
 
 namespace Module
 {
-    [ServiceBinding(typeof(ModulePlayerDying))]
-    public class ModulePlayerDying
+    [ServiceBinding(typeof(PlayerDying))]
+    public class PlayerDying
     {
-        //private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
 
-        public ModulePlayerDying(NativeEventService native, SchedulerService scheduler)
+        // constructor
+        public PlayerDying(NativeEventService nativeEventService, SchedulerService scheduler)
         {
-            native.Subscribe<NwModule, ModuleEvents.OnPlayerDying>(NwModule.Instance, OnPlayerDying);
-            scheduler.ScheduleRepeating(CheckState, TimeSpan.FromSeconds(1));
+            nativeEventService.Subscribe<NwModule, ModuleEvents.OnPlayerDying>(NwModule.Instance, OnPlayerDying);
+            scheduler.ScheduleRepeating(Bleed, TimeSpan.FromSeconds(1));
+
         }
 
-        private static async void OnPlayerDying(ModuleEvents.OnPlayerDying dying)
+        private static void OnPlayerDying(ModuleEvents.OnPlayerDying dying)
         {
+
         }
 
-        private static async Task CheckState()
+        private static void Bleed()
         {
-            Random random = new();
-
-            while (dying.Player.HP < -10)
-            {
-                int dice = random.Next(1, 10);
-                /* 10% chance to stablize */
-                if (dice == 10)
-                {
-                    dying.Player.PlayVoiceChat(VoiceChatType.GuardMe);
-                    dying.Player.HP = 1;
-                    return;
-                }
-                else
-                {
-                    dice = random.Next(1, 5);
-                    ScreamOnDying(dying, dice);
-                    dying.Player.HP--;
-                }
-            }
-            PlayerHasDied(dying);
+            NwModule.Instance.SpeakString(DateTime.Now.ToString(), TalkVolume.Shout);
         }
 
-        private static bool PlayerIsDead(ModuleEvents.OnPlayerDying dying) => dying.Player.HP <= -10;
+        private static bool PlayerIsDead(NwPlayer player) => player.HP <= -10;
 
-        private static void ScreamOnDying(ModuleEvents.OnPlayerDying dying, int dice)
+        private static void ScreamOnDying(NwPlayer player, int dice)
         {
             switch (dice)
             {
-                case 1: dying.Player.PlayVoiceChat(VoiceChatType.Cuss); break;
-                case 2: dying.Player.PlayVoiceChat(VoiceChatType.HealMe); break;
-                case 3: dying.Player.PlayVoiceChat(VoiceChatType.NearDeath); break;
-                case 4: dying.Player.PlayVoiceChat(VoiceChatType.Pain1); break;
-                case 5: dying.Player.PlayVoiceChat(VoiceChatType.Pain2); break;
-                case 6: dying.Player.PlayVoiceChat(VoiceChatType.Pain3); break;
-                default: break;
+                case 1: player.PlayVoiceChat(VoiceChatType.Cuss); break;
+                case 2: player.PlayVoiceChat(VoiceChatType.HealMe); break;
+                case 3: player.PlayVoiceChat(VoiceChatType.NearDeath); break;
+                case 4: player.PlayVoiceChat(VoiceChatType.Pain1); break;
+                case 5: player.PlayVoiceChat(VoiceChatType.Pain2); break;
+                case 6: player.PlayVoiceChat(VoiceChatType.Pain3); break;
             }
         }
 
-        private static void PlayerHasDied(ModuleEvents.OnPlayerDying dying)
+        private static void PlayerHasDied(NwPlayer player)
         {
-            dying.Player.PlayVoiceChat(VoiceChatType.Death);
-            dying.Player.ApplyEffect(EffectDuration.Instant, Effect.VisualEffect(VfxType.ImpDeath));
-            dying.Player.ApplyEffect(EffectDuration.Instant, Effect.Death());
+            player.PlayVoiceChat(VoiceChatType.Death);
+            player.ApplyEffect(EffectDuration.Instant, Effect.VisualEffect(VfxType.ImpDeath));
+            player.ApplyEffect(EffectDuration.Instant, Effect.Death());
         }
     }
 }
