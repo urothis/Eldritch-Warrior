d3a78134de9e7131b9eabcbb7f7f099c914ef9ab
Philip Milliorn <scottmilliorn@outlook.com>
2021-01-26 15:27:31 -0800

    DestroyAllItems updated

--- src/Services/Module/ModuleExtensions.cs
+++ src/Services/Module/ModuleExtensions.cs
@@ -7,13 +7,14 @@ namespace Services.Module
 {
     public static class ModuleExtensions
     {
+        public static bool HasItemByResRef(this NwPlayer player, string nwItem) => player.Items.Any(x => x.ResRef == nwItem);
+
         public static string PrintGPValueOnItem(this NwItem nwItem) => !nwItem.PlotFlag
             ? (nwItem.Description = $"{"Gold Piece Value:".ColorString(new Color(255, 255, 0))}{nwItem.GoldValue.ToString().ColorString(new Color(255, 165, 0))}\n\n{nwItem.OriginalDescription}")
             : nwItem.OriginalDescription;
-
+            
         /* Store hitpoints */
         public static void ClientStoreHitPoints(this NwPlayer player) => player.GetCampaignVariable<int>("Hit_Points", player.Name).Value = player.HP;
         public static void ClientRestoreHitPoints(this NwPlayer player) => player.HP = player.GetCampaignVariable<int>("Hit_Points", player.Name).Value;
-        public static bool HasItemByResRef(this NwPlayer player, string nwItem) => player.Items.Any(x => x.ResRef == nwItem);
     }
 }
