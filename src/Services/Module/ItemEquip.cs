cde58213208623f648392595721b316a3abe5e77
Philip Milliorn <scottmilliorn@outlook.com>
2021-01-26 17:20:46 -0800

    OnPlayerEquipItem

---
+++ src/Services/Module/ItemEquip.cs
@@ -0,0 +1,26 @@
+using System;
+
+//using NLog;
+
+using NWN.API;
+using NWN.API.Constants;
+using NWN.API.Events;
+
+using NWN.Services;
+
+namespace Services.Module
+{
+    [ServiceBinding(typeof(ItemEquip))]
+    public class ItemEquip
+    {
+        //private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        // constructor
+        public ItemEquip(NativeEventService nativeEventService) => nativeEventService.Subscribe<NwModule, ModuleEvents.OnPlayerEquipItem>(NwModule.Instance, OnPlayerEquipItem);
+
+        private static void OnPlayerEquipItem(ModuleEvents.OnPlayerDying dying)
+        {
+            
+        }
+    }
+}
