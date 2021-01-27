403c1169ec91d0a057bd1f63452cbe80bbb00fcf
Philip Milliorn <scottmilliorn@outlook.com>
2021-01-26 17:43:59 -0800

    IsDM

--- src/Services/Module/ItemUnAcquire.cs
+++ src/Services/Module/ItemUnAcquire.cs
@@ -14,6 +14,15 @@ namespace Services.Module
         public ItemUnAcquire(NativeEventService native) =>
             native.Subscribe<NwModule, ModuleEvents.OnUnacquireItem>(NwModule.Instance, OnUnacquireItem);
 
-        private static void OnUnacquireItem(ModuleEvents.OnUnacquireItem unacquireItem) => unacquireItem.Item.PrintGPValueOnItem();
+        private static void OnUnacquireItem(ModuleEvents.OnUnacquireItem unacquireItem)
+        {
+            unacquireItem.Item.PrintGPValueOnItem();
+
+            /* This is to short circuit the rest of this code if we are DM */
+            if (unacquireItem.LostBy is NwPlayer { IsDM: true })
+            {
+                return;
+            }
+        }
     }
 }
