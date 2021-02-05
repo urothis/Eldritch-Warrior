4a4f0d4c343bb77e07045e4c123ca84ca0ef998f
Philip Milliorn <scottmilliorn@outlook.com>
2021-02-04 21:04:35 -0800

    OnExamineObjectBefore finished

--- src/Services/Examine/Examine.cs
+++ src/Services/Examine/Examine.cs
@@ -1,4 +1,4 @@
-using NLog;
+//using NLog;
 
 using NWN.API;
 using NWN.API.Constants;
@@ -12,26 +12,18 @@ namespace Services.Examine
     [ServiceBinding(typeof(ExamineObject))]
     public class ExamineObject
     {
-        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+        //private static readonly Logger logger = LogManager.GetCurrentClassLogger();
 
         public ExamineObject(NWNXEventService nWNX) => nWNX.Subscribe<ExamineEvents.OnExamineObjectBefore>(OnExamineObjectBefore);
 
         public static void OnExamineObjectBefore(ExamineEvents.OnExamineObjectBefore onExamineObject)
         {
-            if (onExamineObject.Examinee is NwCreature creature)
-
-            logger.Info(PrintCRValue(creature));
-
-            /*if (onExamineObject.Examiner.IsReactionTypeHostile((NwCreature)onExamineObject.Examinee))
+            if (onExamineObject.Examinee is NwCreature creature && onExamineObject.Examiner.IsReactionTypeHostile(creature))
             {
-                onExamineObject.Examinee.Description = PrintCRValue((NwCreature)onExamineObject.Examinee);
-            }*/
+                creature.Description = PrintCRValue(creature);
+            }
         }
 
-        private static string PrintCRValue(NwCreature npc)
-        {
-            string description = npc.OriginalDescription;
-            return $"CR Value: {npc.ChallengeRating}\n\nSTR: {npc.GetAbilityScore(Ability.Strength)}\nDEX: {npc.GetAbilityScore(Ability.Dexterity)}\nCON: {npc.GetAbilityScore(Ability.Constitution)}\nINT: {npc.GetAbilityScore(Ability.Intelligence)}\nWIS: {npc.GetAbilityScore(Ability.Wisdom)}\nCHA: {npc.GetAbilityScore(Ability.Charisma)}\nAC: {npc.AC}\nHP: {npc.HP}\nBAB: {npc.BaseAttackBonus}\nFortitude: {npc.GetBaseSavingThrow(SavingThrow.Fortitude)}\nReflex: {npc.GetBaseSavingThrow(SavingThrow.Reflex)}\nWill: {npc.GetBaseSavingThrow(SavingThrow.Will)}\nSR: {npc.SpellResistance}\n\n{description}";
-        }
+        private static string PrintCRValue(NwCreature npc) => $"CR Value: {npc.ChallengeRating}\n\nSTR: {npc.GetAbilityScore(Ability.Strength)}\nDEX: {npc.GetAbilityScore(Ability.Dexterity)}\nCON: {npc.GetAbilityScore(Ability.Constitution)}\nINT: {npc.GetAbilityScore(Ability.Intelligence)}\nWIS: {npc.GetAbilityScore(Ability.Wisdom)}\nCHA: {npc.GetAbilityScore(Ability.Charisma)}\nAC: {npc.AC}\nHP: {npc.HP}\nBAB: {npc.BaseAttackBonus}\nFortitude: {npc.GetBaseSavingThrow(SavingThrow.Fortitude)}\nReflex: {npc.GetBaseSavingThrow(SavingThrow.Reflex)}\nWill: {npc.GetBaseSavingThrow(SavingThrow.Will)}\nSR: {npc.SpellResistance}\n\n{npc.OriginalDescription}";
     }
 }
