cbd56c92b2b9246b6ea5d1f390ad0e520c4bc50c
Philip Milliorn <scottmilliorn@outlook.com>
2021-02-04 18:47:01 -0800

    OnExamineObjectBefore

---
+++ src/Services/Examine/Examine.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+//using NLog;
+
+using NWN.API;
+using NWN.API.Constants;
+using NWN.Services;
+using NWNX.API.Events;
+using NWNX.Services;
+
+namespace Services.Examine
+{
+    [ServiceBinding(typeof(ExamineObject))]
+    public class ExamineObject
+    {
+        //private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        public ExamineObject(NWNXEventService nWNX)
+        {
+            nWNX.Subscribe<ExamineEvents.OnExamineObjectBefore>(OnExamineObjectBefore);
+        }
+
+        public static void OnExamineObjectBefore(ExamineEvents.OnExamineObjectBefore onExamineObject)
+        {
+
+        }
+    }
+}
