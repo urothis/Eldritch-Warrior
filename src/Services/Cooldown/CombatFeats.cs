4f9b737fc75949499b1895e1997c6530ceeac98b
Philip Milliorn <scottmilliorn@outlook.com>
2021-02-10 19:49:24 -0800

    Cooldown

---
+++ src/Services/Cooldown/CombatFeats.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+using NWN.API;
+using NWN.API.Constants;
+using NWN.Services;
+
+using NWNX.API.Events;
+using NWNX.Services;
+
+namespace Services.Cooldown
+{
+    public class CombatFeats
+    {
+
+    }
+}
