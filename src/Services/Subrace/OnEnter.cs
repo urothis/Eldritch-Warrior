1c871c3bca157ba1f6d21dc0abff1c909f0ceaa1
Philip Milliorn <scottmilliorn@outlook.com>
2021-02-14 18:19:46 -0800

    subrace OnEnter

---
+++ src/Services/Subrace/OnEnter.cs
@@ -0,0 +1,20 @@
+//using NLog;
+
+using NWN.API;
+using NWN.API.Events;
+using NWN.Services;
+
+namespace Services.Subrace
+{
+    [ServiceBinding(typeof(OnEnter))]
+    public class OnEnter
+    {
+        //private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+        public OnEnter(NativeEventService nativeEventService) => nativeEventService.Subscribe<NwModule, ModuleEvents.OnClientEnter>(NwModule.Instance, OnClientEnter);
+
+        private static void OnClientEnter(ModuleEvents.OnClientEnter enter)
+        {
+
+        }
+    }
+}
