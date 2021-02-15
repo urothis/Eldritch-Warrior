371d4d5f61a3a0b2157b71b490f82b19dff38bef
Philip Milliorn <scottmilliorn@outlook.com>
2021-02-14 19:21:07 -0800

    finished list

--- src/Services/Subrace/Roster.cs
+++ src/Services/Subrace/Roster.cs
@@ -3,22 +3,61 @@ namespace Services.Subrace
     public static class Roster
     {
         public static readonly string[] dwarf = {
-            "Dwarf-Artic",
-            "Dwarf-Gold",
-            "Dwarf-Gray",
-            "Dwarf-Shield",
-            "Dwarf-Wild",
-            "Dwarf-Urdunnir"
+            "Artic",
+            "Gold",
+            "Gray",
+            "Shield",
+            "Wild",
+            "Urdunnir"
         };
 
         public static readonly string[] elf = {
-            "Elf-Aquatic",
-            "Elf-Avariel",
-            "Elf-Drow",
-            "Elf-Moon",
-            "Elf-Sun",
-            "Elf-Wild",
-            "Elf-Wood"
+            "Aquatic",
+            "Avariel",
+            "Drow",
+            "Moon",
+            "Sun",
+            "Wild",
+            "Wood"
+        };
+
+        public static readonly string[] gnome = {
+            "Deep",
+            "Forest",
+            "Rock"
+        };
+
+        public static readonly string[] orc = {
+            "Gray",
+            "Mountain",
+            "Orog"
+        };
+
+        public static readonly string[] halfling = {
+            "Ghostwise",
+            "Lightfoot",
+            "Strongheart"
+        };
+
+        public static readonly string[] human = {
+            "Calishite",
+            "Chondathan",
+            "Damaran",
+            "Illuskan",
+            "Mulan",
+            "Rashemi",
+            "Tethyrian"
+        };
+
+        public static readonly string[] Planetouched = {
+            "Aasimar",
+            "Genasi-Air",
+            "Genasi-Earth",
+            "Genasi-Fire",
+            "Genasi-Water",
+            "Fey’ri",
+            "Tanarukk",
+            "Tiefling"
         };
     }
 }
