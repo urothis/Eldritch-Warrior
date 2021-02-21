3b9287a78a167c0bbde43598341339bc8ed2b92d
Philip Milliorn <scottmilliorn@outlook.com>
2021-02-21 12:17:03 -0800

    removed SubraceValid

--- src/Services/PlayableRaces/Extensions.cs
+++ src/Services/PlayableRaces/Extensions.cs
@@ -8,19 +8,5 @@ namespace Services.PlayableRaces
 {
     public static class Extensions
     {
-        public static bool SubraceValid(this NwPlayer pc)
-        {
-            return pc.RacialType switch
-            {
-                RacialType.Dwarf when Array.Exists(Roster.dwarf, x => x == pc.SubRace) => true,
-                RacialType.Elf when Array.Exists(Roster.elf, x => x == pc.SubRace) => true,
-                RacialType.Gnome when Array.Exists(Roster.gnome, x => x == pc.SubRace) => true,
-                RacialType.Halfling when Array.Exists(Roster.halfling, x => x == pc.SubRace) => true,
-                RacialType.Human when Array.Exists(Roster.human, x => x == pc.SubRace) => true,
-                RacialType.HalfOrc when Array.Exists(Roster.orc, x => x == pc.SubRace) => true,
-                RacialType.All when Array.Exists(Roster.Planetouched, x => x == pc.SubRace) => true,
-                _ => false
-            };
-        }
     }
 }
