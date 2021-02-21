da4f52c35ee584be5e701835c74381bd91f9c203
Philip Milliorn <scottmilliorn@outlook.com>
2021-02-21 13:08:44 -0800

    refactor engine

--- src/Services/PlayableRaces/Engine.cs
+++ src/Services/PlayableRaces/Engine.cs
@@ -1,18 +1,51 @@
+using System;
 using NWN.API;
 using NWN.API.Events;
-
 using NWN.Services;
+
+using NWNX.API.Events;
+
+using NWNX.Services;
 
 namespace Services.PlayableRaces
 {
-    [ServiceBinding(typeof(OnEnter))]
-    public class OnEnter
+    [ServiceBinding(typeof(Engine))]
+    public class Engine
     {
-        public OnEnter(NativeEventService nativeEventService) => nativeEventService.Subscribe<NwModule, ModuleEvents.OnClientEnter>(NwModule.Instance, OnClientEnter);
-
-        private static void OnClientEnter(ModuleEvents.OnClientEnter onClient)
+        private char wildcard = '!';
+        public Engine(NativeEventService nativeEventService, NWNXEventService nWNX)
         {
+            nativeEventService.Subscribe<NwModule, ModuleEvents.OnClientEnter>(NwModule.Instance, ClientEnter);
+            nativeEventService.Subscribe<NwModule, ModuleEvents.OnPlayerRespawn>(NwModule.Instance, PlayerRespawn);
+            nWNX.Subscribe<LevelEvents.OnLevelUpBefore>(LevelUpBefore);
+        }
 
+        private void LevelUpBefore(LevelEvents.OnLevelUpBefore obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PlayerRespawn(ModuleEvents.OnPlayerRespawn obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ClientEnter(ModuleEvents.OnClientEnter obj)
+        {
+            var pc = obj.Player.SubRace;
+
+            if (String.IsNullOrEmpty(pc))
+            {
+                //We are not a playable race, exit.
+            }
+            else if (pc[0].Equals(wildcard))
+            {
+                //Go to validate subrace an init
+            }
+            else
+            {
+                obj.Player.SendServerMessage($"\n{"ERROR".ColorString(Color.RED)}: The subrace name you have entered doesn't exist.\n");
+            }
         }
     }
 }
