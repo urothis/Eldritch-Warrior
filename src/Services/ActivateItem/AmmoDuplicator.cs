571ab3def8a7464acf55e5777a8150b1bb5b0a57
Philip Milliorn <scottmilliorn@outlook.com>
2021-03-07 23:25:24 -0800

    ActivateItem ammoduper

--- src/Services/ActivateItem/AmmoDuplicator.cs
+++ src/Services/ActivateItem/AmmoDuplicator.cs
@@ -11,15 +11,35 @@ namespace Services.ActivateItem
         public AmmoDuplicator(NativeEventService native) =>
             native.Subscribe<NwModule, ModuleEvents.OnActivateItem>(NwModule.Instance, ActivateItem);
 
-        private void ActivateItem(ModuleEvents.OnActivateItem item)
+        private void ActivateItem(ModuleEvents.OnActivateItem activeItemEvent)
         {
-            NwCreature pc = item.ItemActivator;
-            NwGameObject target = item.TargetObject;
-            switch ()
+            NwItem item = activeItemEvent.ActivatedItem;
+            NwPlayer player = (NwPlayer)activeItemEvent.ItemActivator;
+
+            switch (item.BaseItemType)
             {
+                case BaseItemType.Arrow:
+                case BaseItemType.Bolt:
+                case BaseItemType.Bullet:
+                    item.StackSize = 99;
+                    item.PlotFlag = true;
+                    break;
+                case BaseItemType.Dart:
+                case BaseItemType.Shuriken:
+                case BaseItemType.ThrowingAxe:
+                    item.StackSize = 50;
+                    item.PlotFlag = true;
+                    break;
+                case BaseItemType.Grenade:
+                    item.StackSize = 10;
+                    item.PlotFlag = true;
+                    break;
                 default:
-                break;
+                    player.SendServerMessage($"Invalid Target {item.BaseItemType.ToString().ColorString(Color.WHITE)}".ColorString(Color.ORANGE));
+                    return;
             }
+            
+            player.SendServerMessage($"{item.StackSize.ToString().ColorString(Color.SILVER)} replenished for {item.BaseItemType.ToString().ColorString(Color.WHITE)}");
         }
     }
 }
