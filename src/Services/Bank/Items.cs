fa562f69aa44c9186ca0d818f24aaa620a6e3b3d
Philip Milliorn <scottmilliorn@outlook.com>
2021-03-14 20:48:53 -0700

    Items Close

--- src/Services/Bank/Items.cs
+++ src/Services/Bank/Items.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using NWN.API;
 using NWN.API.Events;
 using NWN.Services;
+
 using NWNX.API;
 
 namespace Services.Bank
@@ -21,26 +22,23 @@ namespace Services.Bank
             if (!obj.ValidateChestLimit())
             {
                 pc.FloatingTextString($"Maximum item count of {Extensions.maxItem} exceeded! Current item count is {obj.Placeable.Inventory.Items.Count<NwItem>()}.");
-                return;
             }
             else if (obj.ItemHasInventory())
             {
                 pc.FloatingTextString("Item with inventories cannot be stored in a bank chest.");
-                return;
             }
             else if (obj.Placeable.Inventory.Items.Any<NwItem>(i => i.PlotFlag))
             {
                 pc.FloatingTextString("Items marked plot are not allowed.");
-                return;
             }
             else if (obj.Placeable.Inventory.Items.Any<NwItem>(i => i.Stolen))
             {
                 pc.FloatingTextString("Stolen items are not allowed.");
-                return;
             }
             else
             {
                 pc.GetCampaignVariable<string>($"{Extensions.bankItem}_{pc.Area.Name}", pc.UUID.ToUUIDString()).Value = obj.Placeable.Serialize();
+                obj.Placeable.Destroy();
             }
         }
     }
