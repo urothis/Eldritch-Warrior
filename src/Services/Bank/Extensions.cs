d21b8f070625ecc6c2babf22c5544677f55433da
Philip Milliorn <scottmilliorn@outlook.com>
2021-03-14 19:31:21 -0700

    ItemHasInventory

--- src/Services/Bank/Extensions.cs
+++ src/Services/Bank/Extensions.cs
@@ -1,12 +1,13 @@
+using System.Linq;
+using NWN.API;
 using NWN.API.Events;
 
 namespace Services.Bank
 {
     public static class Extensions
     {
-        public static bool ValidateItemBankPlaceable(this PlaceableEvents.OnClose obj)
-        {
-            return true;
-        }
+        public static readonly int maxItem = 30;
+        public static bool ValidateChestLimit(this PlaceableEvents.OnClose obj) => obj.Placeable.Inventory.Items.Count<NwItem>() <= maxItem;
+        public static bool ItemHasInventory(this PlaceableEvents.OnClose obj) => obj.Placeable.Inventory.Items.Any<NwItem>(item => item.HasInventory);
     }
 }
--- src/Services/Bank/Items.cs
+++ src/Services/Bank/Items.cs
@@ -1,6 +1,5 @@
-using System;
+using System.Linq;
 using NWN.API;
-using NWN.API.Constants;
 using NWN.API.Events;
 using NWN.Services;
 
@@ -16,10 +15,21 @@ namespace Services.Bank
 
         private void Close(PlaceableEvents.OnClose obj)
         {
-            var pc = obj.LastClosedBy;
+            NwPlayer pc = (NwPlayer)obj.LastClosedBy;
             Location loc = obj.LastClosedBy.Location;
             string name = pc.Name;
             string chestTag = obj.Placeable.GetLocalVariable<string>("CHEST_TAG");
+
+            if (!obj.ValidateChestLimit())
+            {
+                pc.FloatingTextString($"Maximum item count of {Extensions.maxItem} exceeded! Current item count is {obj.Placeable.Inventory.Items.Count<NwItem>()}.");
+                return;
+            }
+            else if (obj.ItemHasInventory())
+            {
+                pc.FloatingTextString("Item with inventories cannot be stored in a bank chest.");
+                return;
+            }
         }
     }
 }
