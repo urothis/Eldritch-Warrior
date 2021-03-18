fb5c5ffd5ed9b117f5133f6ba47592c020abbbc2
Philip Milliorn <scottmilliorn@outlook.com>
2021-03-17 23:35:28 -0700

    IsCorrectItemtype refactored

--- src/Services/ActivateItem/Extensions.cs
+++ src/Services/ActivateItem/Extensions.cs
@@ -8,8 +8,12 @@ namespace Services.ActivateItem
     {
         public static void SocketRunesToItem(this NwItem activatedItem, NwPlayer pc, NwItem item)
         {
+            int IPType = item.GetLocalVariable<int>("IP_TYPE").Value;
+            int IPSubType = item.GetLocalVariable<int>("IP_SUBTYPE").Value;
+            int IPValue = item.GetLocalVariable<int>("IP_VALUE").Value;
+
             //Break script if we try to apply an item property to an item that wont take it.
-            if (!IsCorrectItemtype(pc, item, activatedItem.Name))
+            if (!IsCorrectItemtype(pc, item, IPType))
             {
                 pc.SendServerMessage($"Cannot apply {activatedItem.Name.ColorString(Color.WHITE)} to {item.Name.ColorString(Color.WHITE)}.".ColorString(Color.ORANGE));
                 return;
@@ -17,13 +21,49 @@ namespace Services.ActivateItem
             pc.SendServerMessage($"test good.");
         }
 
-        private static bool IsCorrectItemtype(NwPlayer pc, NwItem item)
+        private static bool IsCorrectItemtype(NwPlayer pc, NwItem item, int IPType)
         {
-            //bank.GetLocalVariable<string>("CHEST_TAG").Value = objectTag;
-            int IPType = item.GetLocalVariable<int>("IP_TYPE").Value;
-            int IPSubType = item.GetLocalVariable<int>("IP_SUBTYPE").Value;
-            int IPValue = item.GetLocalVariable<int>("IP_VALUE").Value;
-
+            switch (IPType)
+            {
+                case 34:
+                case 45:
+                case 61: return IsRangedWeapon(item);
+                case 36:
+                case 6:
+                case 16:
+                case 43:
+                case 56:
+                case 67:
+                case 74:
+                case 82: return IsMeleeWeapon(item);
+                default:
+                    {
+                        if (IsRangedWeapon(item))
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            switch (item.BaseItemType)
+                            {
+                                case BaseItemType.Amulet:
+                                case BaseItemType.Armor:
+                                case BaseItemType.Belt:
+                                case BaseItemType.Boots:
+                                case BaseItemType.Bracer:
+                                case BaseItemType.Cloak:
+                                case BaseItemType.Gloves:
+                                case BaseItemType.Helmet:
+                                case BaseItemType.LargeShield:
+                                case BaseItemType.MagicStaff:
+                                case BaseItemType.SmallShield:
+                                case BaseItemType.Ring:
+                                case BaseItemType.TowerShield: return true;
+                                default: return false;
+                            }
+                        }
+                    }
+            }
         }
 
         private static bool IsRangedWeapon(NwItem item)
@@ -35,10 +75,53 @@ namespace Services.ActivateItem
                 case BaseItemType.Longbow:
                 case BaseItemType.Shortbow:
                     return true;
-                default: return false;
+                default:
+                    return false;
             }
         }
 
+        private static bool IsMeleeWeapon(NwItem item)
+        {
+            switch (item.BaseItemType)
+            {
+                case BaseItemType.Bastardsword:
+                case BaseItemType.Battleaxe:
+                case BaseItemType.Club:
+                case BaseItemType.Dagger:
+                case BaseItemType.DireMace:
+                case BaseItemType.Doubleaxe:
+                case BaseItemType.DwarvenWaraxe:
+                case BaseItemType.Gloves:
+                case BaseItemType.Greataxe:
+                case BaseItemType.Greatsword:
+                case BaseItemType.Halberd:
+                case BaseItemType.Handaxe:
+                case BaseItemType.HeavyFlail:
+                case BaseItemType.Kama:
+                case BaseItemType.Katana:
+                case BaseItemType.Kukri:
+                case BaseItemType.LightFlail:
+                case BaseItemType.LightHammer:
+                case BaseItemType.LightMace:
+                case BaseItemType.Longsword:
+                case BaseItemType.MagicStaff:
+                case BaseItemType.Morningstar:
+                case BaseItemType.Quarterstaff:
+                case BaseItemType.Rapier:
+                case BaseItemType.Scimitar:
+                case BaseItemType.Scythe:
+                case BaseItemType.ShortSpear:
+                case BaseItemType.Shortsword:
+                case BaseItemType.Sickle:
+                case BaseItemType.Trident:
+                case BaseItemType.TwoBladedSword:
+                case BaseItemType.Warhammer:
+                case BaseItemType.Whip:
+                    return true;
+                default:
+                    return false;
+            }
+        }
         public static bool CheckItemIsValidType(this NwItem item)
         {
             switch (item.BaseItemType)
@@ -91,8 +174,10 @@ namespace Services.ActivateItem
                 case BaseItemType.Trident:
                 case BaseItemType.TwoBladedSword:
                 case BaseItemType.Warhammer:
-                case BaseItemType.Whip: return true;
-                default: return false;
+                case BaseItemType.Whip:
+                    return true;
+                default:
+                    return false;
             }
         }
     }
