b2e037a7b2a96052bbfdaf78b63a73fd0eaae8c2
Philip Milliorn <scottmilliorn@outlook.com>
2021-03-28 01:39:50 -0700

    store_infinite

--- src/Services/Store/Infinite.cs
+++ src/Services/Store/Infinite.cs
@@ -1,15 +1,14 @@
 using NWN.API.Events;
-
 using NWN.Services;
+
 namespace Services.Store
 {
     [ServiceBinding(typeof(Infinite))]
 
     public class Infinite
     {
-        public Infinite(ScriptEventService script) => script.SetHandler<StoreEvents.OnOpen>("store_infinite", Open);
-
-        private void Open(StoreEvents.OnOpen obj)
+        [ScriptHandler("store_infinite")]
+        public static void SetupStore(StoreEvents.OnOpen obj)
         {
             foreach (var item in obj.Player.Inventory.Items)
             {
