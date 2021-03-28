2de08ae015efe19648fd1c44fae952aaadde3989
Philip Milliorn <scottmilliorn@outlook.com>
2021-03-28 01:29:40 -0700

    ClientEnter

--- src/Services/PlayableRaces/ClientEnter.cs
+++ src/Services/PlayableRaces/ClientEnter.cs
@@ -1,17 +1,13 @@
-using System;
 using NWN.API;
 using NWN.API.Constants;
-using NWN.API.Events;
 using NWN.Services;
-using NWNX.API.Events;
-using NWNX.Services;
 
 namespace Services.PlayableRaces
 {
-    [ServiceBinding(typeof(Engine))]
-    public class NewBaseType
+    [ServiceBinding(typeof(ClientEnter))]
+    public class ClientEnter
     {
-        private void ClientEnter(ModuleEvents.OnClientEnter obj)
+        public ClientEnter() => NwModule.Instance.OnClientEnter += obj =>
         {
             if (obj.Player.SubRace.Length != 0)
             {
@@ -54,27 +50,6 @@ namespace Services.PlayableRaces
                     obj.Player.SendServerMessage($"{"ERROR".ColorString(Color.RED)}!!! - INVALID SUBRACE NAME.");
                 }
             }
-        }
-    }
-
-    [ServiceBinding(typeof(Engine))]
-    public class Engine : NewBaseType
-    {
-        public Engine(NativeEventService nativeEventService, NWNXEventService nWNX)
-        {
-            nativeEventService.Subscribe<NwModule, ModuleEvents.OnClientEnter>(NwModule.Instance, ClientEnter);
-            nativeEventService.Subscribe<NwModule, ModuleEvents.OnPlayerRespawn>(NwModule.Instance, PlayerRespawn);
-            nWNX.Subscribe<LevelEvents.OnLevelUpBefore>(LevelUpBefore);
-        }
-
-        private void LevelUpBefore(LevelEvents.OnLevelUpBefore obj)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void PlayerRespawn(ModuleEvents.OnPlayerRespawn obj)
-        {
-            throw new NotImplementedException();
-        }
+        };
     }
 }
