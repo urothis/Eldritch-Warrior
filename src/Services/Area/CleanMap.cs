2220ceb059d89608f6f3480d9195263673e18b7e
Scott Milliorn <scottmilliorn@gmail.com>
2021-04-03 22:23:21 -0700

    area_cleanup fixed

--- src/Services/Area/CleanMap.cs
+++ src/Services/Area/CleanMap.cs
@@ -12,23 +12,13 @@ namespace Services.Area
         private static readonly Logger logger = LogManager.GetCurrentClassLogger();
 
         [ScriptHandler("area_cleanup")]
-        public void Exit(CallInfo callinfo)
+        public static void Exit(CallInfo callinfo)
         {
-            logger.Info("1");
-            //Verbose(callinfo);
-            if (callinfo.TryGetEvent(out AreaEvents.OnExit onLeave))
+            if (callinfo.TryGetEvent(out AreaEvents.OnExit onLeave) && !onLeave.Area.FindObjectsOfTypeInArea<NwPlayer>().Any())
             {
-                logger.Info("2");
-                if (!onLeave.Area.FindObjectsOfTypeInArea<NwPlayer>().Any())
-                {
-                    logger.Info("3");
-                    onLeave.CleanupCreaturesAndItems();
-                    logger.Info("4");
-                    onLeave.CleanupPlaceables();
-                    logger.Info("5");
-                    onLeave.CloseDoors();
-                    logger.Info("6");
-                }
+                onLeave.CleanupCreaturesAndItems();
+                onLeave.CleanupPlaceables();
+                onLeave.CloseDoors();
             }
         }
     }
