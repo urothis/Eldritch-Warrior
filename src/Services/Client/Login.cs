4646f6f7ea89a33583301d865f8042945bf117ee
Scott Milliorn <scottmilliorn@gmail.com>
2021-04-16 13:03:29 -0700

    ClientCheckName, WordFilter

--- src/Services/Client/Login.cs
+++ src/Services/Client/Login.cs
@@ -1,7 +1,20 @@
+using NLog;
+
+using NWN.API;
+using NWN.API.Constants;
+using NWN.API.Events;
+using NWN.Services;
+
 namespace Services.Client
 {
+    [ServiceBinding(typeof(Login))]
     public class Login
     {
-        
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        public Login() => NwModule.Instance.OnClientEnter += enter =>
+        {
+
+        };
     }
 }
